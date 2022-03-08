be6c536b5a21ed454c983e6ab39b7d5ac64cef32
Sean Barr <88860713+SeanBarr@users.noreply.github.com>
2022-03-08 16:43:16 -0500

    refactored project

--- src/App.js
+++ src/App.js
@@ -1,69 +1,125 @@
-import React from "react";
-import { useFormik, Field, Formik, Form, ErrorMessage } from "formik";
-import Container from 'react-bootstrap/Container';
+import React from 'react';
+import { Field, Formik } from 'formik';
+import { Container, Button, Row, Col, Form } from 'react-bootstrap';
+import * as Yup from 'yup';
 
 function App() {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      alert("Login Successful");
-    },
-    validate: (values) => {
-      let errors = {};
-      if (!values.email) errors.email = "field required";
-      if (!values.password) errors.password = "field required";
-      return errors;
-    },
-  });
+
 
   return (
-    <div>
-      <main>
-      <Container>
-        <Formik>
-          <Form onSubmit={handleSubmit} >
-        <label htmlFor="emailField">Email:</label>
-            <Field 
-            id="emailField"
-            type="email"
-            name="email"
-            onChange={handleChange}
-            value={formik.values.email}
-            />
-        {errors.email && touched.email ? (
-          <p>
-            {errors.email}
-          </p>
-        ) : null}
-        <ErrorMessage name="email" />
-        <label htmlFor="pswField">Password:</label>
-          <Field
-          id="pswField"
-          type="password"
-          name="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        />
-        {errors.password && touched.password ? (
-          <p>
-            {errors.password}
-          </p>
-        ) : null}
-        <ErrorMessage name="password" />
-        <button id="submitBtn" type="submit">
-          Submit
-        </button>
-      </Form>
-        </Formik>
-          </Container>
-        </main>
-          <footer>
-          </footer>
+    <div className='min-vh-100 d-flex flex-column'>
+      <main className='py-4 d-flex flex-grow-1'>
+        <Container className='d-flex flex-grow-1'>
+          <Formik
+            initialValues={{
+              email: '',
+              password: '',
+            }}
+            validationSchema={Yup.object().shape({
+              email: Yup.string()
+                .email('Email is invalid')
+                .required('Email is required'),
+              password: Yup.string()
+                .min(8, 'Password must be at least 8 charaters')
+                .required('Password is required'),
+            })}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              alert('Login was Successful')
+              console.log(values)
+              setSubmitting(false)
+              console.log(resetForm())
+            }}
+            onReset={({ resetForm }) => {
+              resetForm()
+            }}
+          >
+            {({
+              handleSubmit,
+              handleReset,
+              handleChange,
+              handleBlur,
+              values,
+              touched,
+              errors,
+              isSubmitting,
+            }) => (
+              <div className='w-form m-auto'>
+                <h1 className='mb-4'>
+                  Formik <span className='text-danger'>Form</span>
+                </h1>
+                <Form
+                  onSubmit={handleSubmit}
+                  onReset={handleReset}
+                  className='px-3 py-4 shadow-lg rounded'
+                  noValidate
+                >
+                  <Form.Group className='mb-4' controlId='email'>
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control
+                      as={Field}
+                      type='email'
+                      name='email'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email || ''}
+                      isValid={touched.email && !errors.email}
+                      isInvalid={touched.email && errors.email}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                      {errors.email}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className='mb-4' controlId='password'>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      as={Field}
+                      type='password'
+                      name='password'
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password || ''}
+                      isValid={touched.password && !errors.password}
+                      isInvalid={touched.password && errors.password}
+                    />
+                    <Form.Control.Feedback type='invalid'>
+                      {errors.password}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Row xs={1} sm={2} className='g-2 mt-0'>
+                    <Col>
+                      <Button
+                        className='w-100'
+                        variant='dark'
+                        type='submit'
+                        disabled={isSubmitting}
+                      >
+                        Submit
+                      </Button>
+                    </Col>
+                    <Col>
+                      <Button
+                        className='w-100'
+                        variant='danger'
+                        type='reset'
+                        disabled={isSubmitting}
+                      >
+                        Reset
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              </div>
+            )}
+          </Formik>
+        </Container>
+      </main>
+      <footer className='py-3 bg-dark'>
+        <Container>
+          <p className='m-0 text-light text-center'>Made by Sean Barr</p>
+        </Container>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
